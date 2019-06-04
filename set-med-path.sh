e30a05625b29b46f75413c25bfbc2ced946557d8
U-MEDACUITY\mpolia <mpolia@MPOLIA-SOFL1.medacuity.local>
2019-06-04 09:53:00 -0400

    Add path enhancement scripts.

---
+++ set-med-path.sh
@@ -0,0 +1,4 @@
+
+
+export PATH=/c/Users/mpolia/workspace/4_8-2013q4-major/bin:/c/Users/mpolia/workspace/4_8-2013q4-major/arm-none-eabi/bin:/c/Users/mpolia/workspace/stlink/build/Debug:/c/Users/mpolia/workspace/OpenOCD-20181130/bin:$PATH
+
---
+++ set-mpolia-path.sh
@@ -0,0 +1,4 @@
+
+
+export PATH=/home/mpolia/WORKSPACE/4_8-2013q4-major/bin:/home/mpolia/WORKSPACE/4_8-2013q4-major/arm-none-eabi/bin:/home/mpolia/WORKSPACE/stlink/build/Debug:/home/mpolia/WORKSPACE/OpenOCD-20181130/bin:$PATH
+
